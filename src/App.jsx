5f02d8ae5958610ea99d2e3bce2ada72f7cba962
Mubasshir08 <msr4212019@gmail.com>
2025-02-16 12:31:50 +0600

    navbar added & toggle btn added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import Route from './routes/Route';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import Navbar from './shared/navbar/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <RouterProvider router={Route}/>
     </div>
   )
