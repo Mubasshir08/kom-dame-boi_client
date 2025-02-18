e23709ed3233bad0764464dfd877437c9fd3cfa6
Mubasshir08 <msr4212019@gmail.com>
2025-02-18 23:08:06 +0600

    reader posts, sell of books, buy book navigation added

--- src/shared/menu-bar/MenuBar.jsx
+++ src/shared/menu-bar/MenuBar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { MdOutlineFeed, MdOutlineShoppingBag } from 'react-icons/md'
 
 const MenuBar = () => {
   return (
     <ul className="menu bg-base-200 rounded-box md:hidden -translate-y-80">
   <li>
-    <a className="tooltip tooltip-right" data-tip="Post">
+    <Link to = '/reader/post' className="tooltip tooltip-right" data-tip="Post">
     <MdOutlineFeed className='h-5 w-5' />
-    </a>
+    </Link>
   </li>
   <li>
-    <a className="tooltip tooltip-right" data-tip="Buy">
+    <Link to = '/reader/buy' className="tooltip tooltip-right" data-tip="Buy">
     <MdOutlineShoppingBag className='h-5 w-5' />
-    </a>
+    </Link>
   </li>
   <li>
-    <a className="tooltip tooltip-right" data-tip="Sell">
+    <Link to = '/reader/sell' className="tooltip tooltip-right" data-tip="Sell">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-5 w-5"
@@ -28,7 +29,7 @@ const MenuBar = () => {
           strokeWidth="2"
           d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
       </svg>
-    </a>
+    </Link>
   </li>
 </ul>
   )
