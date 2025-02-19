4024786013a6a3e147c7adccc01deec047ed3aa1
Mubasshir08 <msr4212019@gmail.com>
2025-02-19 14:45:31 +0600

    make signup page responsive with google auth button

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -52,7 +52,7 @@ const Signup = () => {
 
   return (
     <div className="min-w-[500px] ">
-      <div className="border-2 w-1/2 mx-auto my-5 rounded-2xl">
+      <div className="border-2 w-6/7 md:w-1/2 mx-auto my-5 rounded-2xl">
         <h1 className="text-3xl font-bold text-primary text-center mt-5 mb-3">Signup</h1>
         <form className="p-4">
           <div className="flex flex-col items-center my-1">
@@ -155,7 +155,7 @@ const Signup = () => {
             </div>
           </div>
 
-          <div className="w-[40%] mb-2 flex justify-between">
+          <div className="w-[63%] md:w-[40%] mb-4 md:mb-2 flex justify-between">
             <h3 className="text-primary">Already have an account?</h3>
             <Link to="/login" className="text-blue-500">Sign In</Link>
           </div>
@@ -168,6 +168,11 @@ const Signup = () => {
               Sign Up
             </button>
           </div>
+          {/* Google */}
+          <button className="btn btn-block bg-white text-black border-[#e5e5e5] mt-3">
+            <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+            Login with Google
+          </button>
         </form>
       </div>
     </div>
