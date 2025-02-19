1a39f650c1642bd53fa74e0c11b4a24e5baff3e2
Mubasshir08 <msr4212019@gmail.com>
2025-02-19 21:52:55 +0600

    post pop-up added

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Card from '../shared/card/Card'
+import Post from '../shared/Post'
 
 const Feed = () => {
   return (
     <div className='container px-3 mb-3'>
+      <Post />
       <Card />
       <Card />
       <Card />
---
+++ src/shared/Post.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className='w-1/2 absolute bg-base-100 border border-neutral z-10 rounded-md translate-x-77'>
+      <form action="" className='px-3'>
+        <h3 className='text-center text-lg'>Post</h3>
+        <select className="border-none focus:outline-none bg-primary text-primary-content rounded-md ml-10 mt-5 p-1">
+          <option defaultChecked = {true}>Sell</option>
+          <option>Regular</option>
+        </select>
+        <textarea name="" id="" className='resize-none border-none focus:outline-none w-7/8 ml-10 my-3' placeholder='Description ...'></textarea>
+        <button className="btn btn-neutral w-7/8 ml-10 my-5">Post</button>
+      </form>
+    </div>
+  )
+}
+
+export default Post
+
