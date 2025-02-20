40e1a208f255af7401206f787e8a75eb67236a2d
Mubasshir08 <msr4212019@gmail.com>
2025-02-20 12:27:12 +0600

    add close button on post pop-up

--- src/shared/Post.jsx
+++ src/shared/Post.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPopup } from '../redux/PostSlice';
 
 const Post = () => {
+  const dispatch = useDispatch();
+    const {popup} = useSelector((state) => state.post);
+    // console.log(popup)
+    const popupHandler = (e) => {
+      e.preventDefault();
+      dispatch(setPopup(!popup));
+    }
   return (
     <div className='w-1/2 absolute bg-base-100 border border-neutral z-10 rounded-md translate-x-77'>
-      <form action="" className='px-3'>
+      <form action="" className='px-3 relative'>
         <h3 className='text-center text-lg'>Post</h3>
+        <button className="btn btn-neutral btn-circle absolute top-2 right-3 " onClick={popupHandler}>x</button>
         <select className="focus:outline-none border-primary rounded-md ml-10 mt-5 p-1">
           <option defaultChecked = {true}>Sell</option>
           <option>Regular</option>
