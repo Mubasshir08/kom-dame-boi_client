f6245e3f6b3eafc307876fe02799c2761cef4bd8
Mubasshir08 <msr4212019@gmail.com>
2025-02-20 13:41:06 +0600

    textarea design fix for mobile devices

--- src/shared/Post.jsx
+++ src/shared/Post.jsx
@@ -24,9 +24,9 @@ const Post = () => {
           <option defaultChecked = {true}>Sell</option>
           <option>Regular</option>
         </select>
-        <div className='relative border border-dotted w-7/8 ml-5.5 md:ml-10 my-3 md:p-1 md:h-50'>
-        <textarea name="" id="" className='resize-none border-none focus:outline-none w-full h-37 overflow-scroll' placeholder='Description ...'></textarea>
-        <GrAttachment className='absolute bottom-2 left-3 text-lg cursor-pointer'/>
+        <div className='relative border border-dotted w-7/8 ml-5.5 md:ml-10 my-3 md:p-1 h-40 md:h-50'>
+        <textarea name="" id="" className='resize-none border-none focus:outline-none w-full h-30 p-1 md:h-37 overflow-scroll' placeholder='Description ...'></textarea>
+        <GrAttachment className='absolute bottom-2 left-2 md:left-3 text-lg cursor-pointer'/>
         </div>
         <button className="btn btn-neutral w-7/8 ml-5.5 md:ml-10 mb-5">Post</button>
       </form>
