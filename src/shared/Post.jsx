016e4f06732e414ac1a29d28d7db2c9a71829e6b
Mubasshir08 <msr4212019@gmail.com>
2025-02-20 13:37:59 +0600

    textarea design fix

--- src/shared/Post.jsx
+++ src/shared/Post.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPopup } from '../redux/PostSlice';
+import { GrAttachment } from 'react-icons/gr';
 
 const Post = () => {
     const [IsPopUpShow, setIsPopUpShow] = useState(false);
@@ -15,16 +16,19 @@ const Post = () => {
       setIsPopUpShow(popup);
     },[popup])
   return (
-    <div className={`${IsPopUpShow ? "block" : "hidden"} w-1/2 absolute bg-base-100 border border-neutral z-10 rounded-md translate-x-77`}>
-      <form action="" className='px-3 relative'>
+    <div className={`${IsPopUpShow ? "block" : "hidden"} w-2/3 md:w-1/2 pt-4 md:pt-2 absolute bg-base-100 border border-neutral z-10 rounded-md translate-x-20 md:translate-x-77`}>
+      <form action="" className='md:px-3 relative'>
         <h3 className='text-center text-lg'>Post</h3>
-        <button className="btn btn-neutral btn-circle absolute top-2 right-3 " onClick={popupHandler}>x</button>
-        <select className="focus:outline-none border-primary rounded-md ml-10 mt-5 p-1">
+        <button className="btn btn-neutral btn-circle absolute -top-2 md:top-1 right-3 " onClick={popupHandler}>x</button>
+        <select className="focus:outline-none border-primary rounded-md ml-5.5 md:ml-10 mt-5 p-1">
           <option defaultChecked = {true}>Sell</option>
           <option>Regular</option>
         </select>
-        <textarea name="" id="" className='resize-none border-none focus:outline-none w-7/8 ml-10 my-3' placeholder='Description ...'></textarea>
-        <button className="btn btn-neutral w-7/8 ml-10 my-5">Post</button>
+        <div className='relative border border-dotted w-7/8 ml-5.5 md:ml-10 my-3 md:p-1 md:h-50'>
+        <textarea name="" id="" className='resize-none border-none focus:outline-none w-full h-37 overflow-scroll' placeholder='Description ...'></textarea>
+        <GrAttachment className='absolute bottom-2 left-3 text-lg cursor-pointer'/>
+        </div>
+        <button className="btn btn-neutral w-7/8 ml-5.5 md:ml-10 mb-5">Post</button>
       </form>
     </div>
   )
