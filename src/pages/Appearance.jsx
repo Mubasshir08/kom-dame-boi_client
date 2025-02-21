3109ac2da25a36f08a61f53a17a987018908a650
Mubasshir08 <msr4212019@gmail.com>
2025-02-21 11:40:19 +0600

    theme checkbox added on theme page

--- src/pages/Appearance.jsx
+++ src/pages/Appearance.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Appearance = () => {
   return (
-    <div>Appearance</div>
+    <div className='flex'>
+      <h3>Appearance:</h3>
+      <label htmlFor="light">light
+        <input type="checkbox" className="checkbox" id='light'/>
+      </label>
+      <label htmlFor="dark">dark
+        <input type="checkbox" className="checkbox" id='dark'/>
+      </label>
+    </div>
   )
 }
 
