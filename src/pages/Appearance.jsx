7cd7c71ce2914310a3016ff3d6b5c9db5b5dfca4
Mubasshir08 <msr4212019@gmail.com>
2025-02-22 07:21:51 +0600

    theme change functionality added

--- src/pages/Appearance.jsx
+++ src/pages/Appearance.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Appearance = () => {
+  const [islight, setIsLight] = useState(true);
+  const handleThemeChange = () => {
+    setIsLight(!islight);
+  }
   return (
     <div className='flex'>
       <h3>Appearance:</h3>
       <label htmlFor="light">light
-        <input type="checkbox" className="checkbox" id='light'/>
+        <input type="checkbox" className="checkbox theme-controller" id='light' value={"caramellatte"} checked={islight} onClick={handleThemeChange}/>
       </label>
       <label htmlFor="dark">dark
-        <input type="checkbox" className="checkbox" id='dark'/>
+        <input type="checkbox" className="checkbox theme-controller" id='dark' value={"black"} checked={!islight} onClick={handleThemeChange}/>
       </label>
     </div>
   )
