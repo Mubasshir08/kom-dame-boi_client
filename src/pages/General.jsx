d1777d2a720cf9e52a93afad025b644ed8cddfc4
Mubasshir08 <msr4212019@gmail.com>
2025-02-22 08:25:04 +0600

    input field tooltip added

--- src/pages/General.jsx
+++ src/pages/General.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const General = () => {
+  const [userName, setUserName] = useState("H");
   return (
-    <div>General</div>
+    <div className='w-1/3 ml-5'>
+      <div className='py-3'>
+        <h3 className='pb-2'>Full Name</h3>
+        <div className="tooltip tooltip-right" data-tip="can't be changed">
+          <input type="text" disabled placeholder={"Mubasshir"} className='w-80 input input-primary focus:outline-none' />
+        </div>
+      </div>
+      <div className='flex flex-col py-3'>
+        <h3 className='pb-2'>User Name</h3>
+        <input type="text" value={userName} className='input input-primary focus:outline-none' onChange={(e) => setUserName(e.target.value)} />
+      </div>
+      <div className='flex flex-col'>
+        <h3 className='pb-2'>Password</h3>
+        <input type="text" value={userName} className='input input-primary focus:outline-none' onChange={(e) => setUserName(e.target.value)} />
+      </div>
+    </div>
   )
 }
 
